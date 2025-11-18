48e6cf4a5605ea43b759da3f2567fbe5ed258ff0
andyrudovv <110022906+andyrudovv@users.noreply.github.com>
2025-11-18 14:38:19 +0500

    new structure

---
+++ backend/ai/augmentation.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.layers import RandomRotation, RandomZoom, RandomContrast
+from tensorflow.keras.layers import Input
+
+def augment_input(input_layer):
+    x = RandomRotation((-0.2, 0.2))(input_layer)
+    x = RandomZoom(0.1, 0.1)(x)
+    x = RandomContrast((0.8, 1.2))(x)
+    return x
